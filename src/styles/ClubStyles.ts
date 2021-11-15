ea850a214cb6a7c02d900899fc21ab89d4fcee8b
Bruno Haetinger <brunohaetinger@gmail.com>
2021-11-14 21:24:33 -0300

    Add ClubStyle for shadows

---
+++ src/styles/ClubStyles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+export const clubStyles = StyleSheet.create({
+  shadow: {
+    shadowOffset: {width: 2, height: 2},
+    shadowColor: 'black',
+    shadowOpacity: 0.8,
+    shadowRadius: 3
+  }
+});
